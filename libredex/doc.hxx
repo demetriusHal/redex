449083de1206e10e2917d7e55ee34e277de9e34a
Vasilis Samoladas <vsam@softnet.tuc.gr>
2018-07-28 12:32:35 +0300

    Starting the documentation

---
+++ libredex/doc.hxx
@@ -0,0 +1,104 @@
+
+/**
+	\defgroup LIBREDEX The libredex code
+	\brief The main dex manipulation library
+
+	@{
+*/
+
+/** \class RedexContext
+	\brief Context singleton object's class
+	\see g_redex
+*/
+
+/** \var extern RedexContext* g_redex 
+	\brief Global Redex context object
+
+	\see RedexContext
+*/
+
+
+/** \class DexClass */
+/** \class DexDebugInstruction */
+/** \class DexOutputIdx */
+/** \class DexString */
+/** \class DexType */
+/** \typedef Scope = std::vector<DexClass*> */
+
+/**
+  @}	
+*/
+
+/** \file ApkManager.h */
+/** \file CallGraph.h */
+/** \file ClassHierarchy.h */
+/** \file ConcurrentContainers.h */
+/** \file ConfigFiles.h */
+/** \file ControlFlow.h */
+/** \file Creators.h */
+/** \file Dataflow.h */
+/** \file Debug.h */
+/** \file DexAccess.h */
+/** \file DexAnnotation.h */
+/** \file DexAsm.h */
+/** \file DexClass.h */
+/** \file DexDebugInstruction.h */
+/** \file DexIdx.h */
+/** \file DexInstruction.h */
+/** \file DexLoader.h */
+/** \file DexMemberRefs.h */
+/** \file DexOpcode.h */
+/** \file DexOutput.h */
+/** \file DexPosition.h */
+/** \file DexStore.h */
+/** \file DexUtil.h */
+/** \file Gatherable.h */
+/** \file ImmutableSubcomponentAnalyzer.h */
+/** \file Inliner.h */
+/** \file InstructionAnalyzer.h */
+/** \file InstructionLowering.h */
+/** \file IRAssembler.h */
+/** \file IRCode.h */
+/** \file IRInstruction.h */
+/** \file IRList.h */
+/** \file IROpcode.h */
+/** \file IRTypeChecker.h */
+/** \file JarLoader.h */
+/** \file Match.h */
+/** \file MethodDevirtualizer.h */
+/** \file Mutators.h */
+/** \file Pass.h */
+/** \file PassManager.h */
+/** \file PassRegistry.h */
+/** \file PluginRegistry.h */
+/** \file PointsToSemantics.h */
+/** \file PointsToSemanticsUtils.h */
+/** \file PrintSeeds.h */
+/** \file ProguardConfiguration.h */
+/** \file ProguardLexer.h */
+/** \file ProguardMap.h */
+/** \file ProguardMatcher.h */
+/** \file ProguardParser.h */
+/** \file ProguardPrintConfiguration.h */
+/** \file ProguardRegex.h */
+/** \file ProguardReporting.h */
+/** \file ReachableClasses.h */
+/** \file ReachableObjects.h */
+/** \file RedexContext.h */
+/** \file RedexResources.h */
+/** \file ReferencedState.h */
+/** \file Resolver.h */
+/** \file Show.h */
+/** \file SimpleReflectionAnalysis.h */
+/** \file SimpleValueAbstractDomain.h */
+/** \file StringBuilder.h */
+/** \file Timer.h */
+/** \file Trace.h */
+/** \file Transform.h */
+/** \file TypeSystem.h */
+/** \file Vinfo.h */
+/** \file VirtualScope.h */
+/** \file Walkers.h */
+/** \file Warning.h */
+/** \file WorkQueue.h */
+
