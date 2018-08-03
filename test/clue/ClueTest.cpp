f5ecf809f1873dd11cb3b6e966a89ad4fe15625c
Vasilis Samoladas <vsam@softnet.tuc.gr>
2018-08-03 18:54:51 +0300

    Added some functionality to ClueTest.h that allows tests to be written relatively easily. A demo of the added functionality appears in the SampleTest.cpp file.

--- test/clue/ClueTest.cpp
+++ test/clue/ClueTest.cpp
@@ -1,28 +1,85 @@
-#include <sstream>
+
+#include <fstream>
 
 #include "ClueTest.h"
 #include "DexLoader.h"
+#include "JarLoader.h"
 
-DexClasses& ClueTest::load_classes(const std::string& dexfile)
+#include <boost/exception/diagnostic_information.hpp>
+
+
+ClueTest::ClueTest()
+	: config(Json::nullValue)
+{ 
+	DexStore root("classes");
+	m_stores.emplace_back(std::move(root));
+}
+
+ClueTest::~ClueTest()
+{ }
+
+
+void ClueTest::load_dex(const std::string& dexfile)
 {
-	DexMetadata dm;
+	DexClasses classes;	
+	try {
+	 	classes = load_classes_from_dex(dexfile.c_str());
+	} 
+	catch(...) {
+		// prints diagnostic and rethrows 
+	    std::cerr <<
+	        "Unexpected exception, diagnostic information follows:\n" <<
+    	    boost::current_exception_diagnostic_information();
+		throw;
+	}
+	root_store().add_classes(std::move(classes));
+}
 
-	std::ostringstream dex_id;
-	dex_id << "classes";
-	if(stores.size()>0)
-		dex_id << stores.size()+1;
-	std::string dexid = dex_id.str();
-	dm.set_id(dexid);
 
-	std::cout << "Metadata: " << dm.get_id() << std::endl;
+void ClueTest::load_jar(const std::string& jarfile)
+{
+	if(!load_jar_file(jarfile.c_str(), &m_external_classes)) {
+		std::cerr << "Loading of jar file '" << jarfile << "' failed!" << std::endl;
+		throw std::runtime_error(__FUNCTION__);
+	}
+}
 
-	DexStore store(dm);
-	DexClasses classes = load_classes_from_dex(dexfile.c_str());
-	store.add_classes(std::move(classes));
-	stores.emplace_back(std::move(store));
+void ClueTest::run_passes(const std::vector<Pass*>& passes,
+              const redex::ProguardConfiguration& pg_config,
+              bool verify_none_mode, bool is_art_build)
+{
+	ConfigFiles cfg(config);
+	// N.B. Here, we would call apply_deobfuscated_names when ProGuard map files are
+	// provided!
 
-	DexClasses& cls = stores.back().get_dexen().back();
-	std::cout << "Loaded classes: " << cls.size() << std::endl ;
-	return cls;
+	PassManager manager(passes, pg_config, config, verify_none_mode, is_art_build);
+	// This will run passes that check the manager.no_proguard_rules() guard.
+	// If not called, passes that require proguard "keep rules" would be skipped
+	manager.set_testing_mode();
+	manager.run_passes(m_stores, m_external_classes, cfg);
+}
+
+
+void ClueTest::parse_config(const char* js) 
+{
+  std::string jss(js);
+  Json::Reader reader;
+  reader.parse(jss, config);
+  if(! reader.good()) {
+    std::string what = "Json_parse errors: " + reader.getFormattedErrorMessages();
+    throw std::runtime_error(what.c_str());
+  }
+}
+
+void ClueTest::load_config(const std::string& cfgfile) 
+{
+  std::ifstream cfgin;
+  cfgin.open(cfgfile.c_str(), std::ifstream::in);
+    Json::Reader reader;
+  reader.parse(cfgin, config);
+  if(! reader.good()) {
+    std::string what = "Json_parse errors: " + reader.getFormattedErrorMessages();
+    throw std::runtime_error(what.c_str());
+  }
 }
 
--- test/clue/SampleTest.cpp
+++ test/clue/SampleTest.cpp
@@ -28,22 +28,120 @@ TEST(SampleTests, sample1) {
 }
 
 
+class SamplePass : public Pass 
+{
+public:
+  SamplePass() : Pass("SamplePass") { }
+
+  std::string param;
+
+  size_t configure_calls = 0;
+  size_t eval_calls = 0;
+  size_t run_calls = 0;
+
+  virtual void configure_pass(const PassConfig& pc) override {
+    pc.get("foo", "void", param);
+
+    configure_calls++; 
+  }
+
+  virtual void eval_pass(DexStoresVector&, ConfigFiles&, PassManager&) override {
+    eval_calls++;
+  }
+
+  virtual void run_pass(DexStoresVector&, ConfigFiles&, PassManager&) override {
+    run_calls++;
+  }
+
+};
+
+
+
+
 /**
   @fn ClueTest_sample2_Test
   @brief A sample test case for clue-redex.
 
   The purpose of this test function is to give a guide into writing your own test functions.
   In this example, the test uses fixture @ref ClueTest  and `sample2` is the name of
-  the test case.
+  the test case. A trivial, user-defined pass is then executed.
   */
 TEST_F(ClueTest, sample2) {
 
-  // This test is a self-contained routine, that does nothing
-  ASSERT_EQ(1,1);
-  
-  ASSERT_EQ(0, stores.size());
+  using std::cout;
+  using std::endl;
 
-  DexClasses& classes = load_classes("sample-classes");
+  // This test is a self-contained routine, that loads a simple .dex file
+  // and checks what it loaded
 
+  // initially the fixtures store is empty
+  ASSERT_EQ(1, stores().size());
+  EXPECT_EQ("classes", root_store().get_name());
+
+  // Load the sample-classes.dex file by calling ClueTest::load_dex()
+  load_dex("sample-classes.dex");
+
+  // Make some simple checks
+  ASSERT_EQ(1, root_store().get_dexen().size());
+  const DexClasses& classes = root_store().get_dexen().back();
   ASSERT_EQ(1, classes.size());
+  ASSERT_EQ("Lclue/testing/SampleClass;", classes[0]->get_name()->str());
+
+  //--------------------
+  // Execute a run with a single pass
+  //--------------------
+
+  auto sample_pass = new SamplePass();
+  std::vector<Pass*> passes { sample_pass };
+  run_passes(passes);
+
+  // Note that configure_pass was not called. 
+  // This is because the PassManager was not configured, therefore it
+  // just ran each pass in array `passes` unconfigured.
+  EXPECT_EQ(0, sample_pass->configure_calls);
+
+  EXPECT_EQ(1, sample_pass->eval_calls);
+  EXPECT_EQ(1, sample_pass->run_calls);
+
+  //----------------------
+  // An execution with several executions of the pass. This is excercising
+  // the PassManager feature that allows a pass to be invoked multiple times
+  // in the same run.
+  //------------------------
+
+  //
+  // Use the config to execute the SamplePass pass twice.
+  //
+  CLUETEST_CONFIG(
+  {
+      "redex": {
+        "passes": [
+            "SamplePass#1",
+            "SamplePass#2"
+          ]
+      },
+      "SamplePass#1": { // Note: you can add a comment
+        "foo": "bar"
+      },
+      "SamplePass#2": { // Note: another comment
+        "foo": "baz",
+        "foof": null
+      }
+  }
+  );
+  
+  std::cerr << config << std::endl;
+
+  // reset counters in the pass object (dirty hack :-)
+  *sample_pass = SamplePass();
+
+  run_passes(passes);
+
+  EXPECT_EQ(2, sample_pass->configure_calls);
+  EXPECT_EQ(2, sample_pass->eval_calls);
+  EXPECT_EQ(2, sample_pass->run_calls);
+
+  // Note that the param has taken the value provided at the last activation,
+  // i.e., that corresponding to "SamplePass#2"
+  EXPECT_EQ("baz", sample_pass->param);
 }
